8fbc991e88416d417625ab6292e4f198adc921b9
Sasha <sasha.luhr@gmail.com>
2021-09-06 01:12:31 +0200

    Added css¨

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chess
+
+Chess game
--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //
+
+const squares = [...document.querySelectorAll('.square')];
+console.log(squares);
+squares.forEach(square => {
+    const id = document.createElement('span');
+    id.textContent = square.id;
+    console.log(square.id);
+    square.appendChild(id);
+})
